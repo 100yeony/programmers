54690ad24d8279d4f8d7a1b62300400b689edd8e
100yeony <byh741@naver.com>
2022-07-12 17:18:28 +0900

    해시1번 문제 풀이(dictionary, Counter 클래스 이용)

--- 해시1.py
+++ 해시1.py
@@ -1,1 +1,25 @@
-print('hello')
+from collections import Counter
+
+participant = ["leo", "kiki", "eden"]
+completion = ["eden", "kiki"]
+final = []
+
+def solution(participant, completion):
+    result = Counter(participant).most_common()
+
+    result_dict = {}
+    for i in result:
+        # result_dict = {i[0]:i[1]}
+        result_dict[i[0]]=i[1]
+    # print('first', result_dict)
+
+    for i in completion:
+        if i in result_dict:
+            result_dict[i] -= 1
+    # print('second', result_dict)
+
+    for k,v in result_dict.items():
+        if v != 0:
+            return k
+
+solution(participant, completion)
